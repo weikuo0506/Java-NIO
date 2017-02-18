53ac2a8687aa9c0f337c8e145c7d0df625ad6539
walkerwei <walkerwei@fenqile.com>
2017-02-18 20:29:23 +0800

    继续

--- src/main/java/com/walker/nio/netty_demo/DiscardServer.java
+++ src/main/java/com/walker/nio/netty_demo/DiscardServer.java
@@ -15,48 +15,55 @@ import lombok.extern.slf4j.Slf4j;
  * @author walkerwei
  * @version 2017/2/16
  */
+
+/**
+ * Discards any incoming data.
+ */
 @Slf4j
 public class DiscardServer {
+
     private int port;
 
     public DiscardServer(int port) {
         this.port = port;
     }
 
-    public void run(){
-        EventLoopGroup boss = new NioEventLoopGroup();
-        EventLoopGroup worker = new NioEventLoopGroup();
-        try{
-            ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(boss, worker)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
+    public void run() throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
                         @Override
-                        protected void initChannel(SocketChannel ch) throws Exception {
+                        public void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline().addLast(new DiscardServerHandler());
                         }
                     })
-                    .option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
 
             // Bind and start to accept incoming connections.
-            ChannelFuture future = bootstrap.bind(port).sync();
+            ChannelFuture f = b.bind(port).sync(); // (7)
 
             // Wait until the server socket is closed.
-            future.channel().closeFuture().sync();
-
-
-        } catch (Exception e) {
-            log.error("exception = {}", e);
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
         } finally {
-            worker.shutdownGracefully();
-            worker.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
         }
     }
 
-    public static void main(String[] args) {
-        new DiscardServer(8080).run();
-        log.info("discard server started!");
-        System.out.println("over");
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new DiscardServer(port).run();
     }
 }
--- src/main/java/com/walker/nio/netty_demo/DiscardServerHandler.java
+++ src/main/java/com/walker/nio/netty_demo/DiscardServerHandler.java
@@ -9,17 +9,21 @@ import lombok.extern.slf4j.Slf4j;
  * @author walkerwei
  * @version 2017/2/16
  */
+
+/**
+ * Handles a server-side channel.
+ */
 @Slf4j
-public class DiscardServerHandler extends ChannelInboundHandlerAdapter  {
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
         // Discard the received data silently.
         ((ByteBuf) msg).release(); // (3)
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
         // Close the connection when an exception is raised.
         cause.printStackTrace();
         ctx.close();
